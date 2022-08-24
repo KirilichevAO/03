1987b90241820f6bbf05e8445b62e2ae1275bef5
KirilichevAO <kirilichev.a@icloud.com>
2022-08-24 15:52:50 +0700

    22

---
+++ 22/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1, 4
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void PrintQuart(int num)
+{
+    for (int i = 1; i <= num; i++)
+    Console.WriteLine($"{i} \t {i*i}");
+}
+if (number < 1)
+{
+    Console.WriteLine("Введите число > 1");
+    return;
+}
+PrintQuart(number);
