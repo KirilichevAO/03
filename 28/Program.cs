2db725fd38f32ca792b52d0ee967e06a2a4db5b3
KirilichevAO <kirilichev.a@icloud.com>
2022-08-24 15:53:36 +0700

    27

---
+++ 28/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int CompositionNumber(int num)
+{
+    int compos = 1; 
+    for (int i = 1; i <= num; i++)
+    {
+        compos = compos * i;
+    }
+    return compos;
+}
+int result = CompositionNumber(number);
+Console.WriteLine($"{number} -> {result}");
