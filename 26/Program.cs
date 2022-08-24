a96e05b8459fbe010bf7bbe79d45820da88bbc36
KirilichevAO <kirilichev.a@icloud.com>
2022-08-24 15:53:23 +0700

    26

---
+++ 26/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NumbersOfDigit(int num)
+{
+    int count = 0;
+    if (num == 0) return 1;
+    while (num > 0)
+    {
+        count++;
+        num = num / 10;
+    }
+    return count;
+}
+int result = NumbersOfDigit(number);
+Console.WriteLine($"{number} -> {result}");
