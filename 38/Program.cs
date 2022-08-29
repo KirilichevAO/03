597e61ef98b1d13902697831703b5cf66176f5f6
KirilichevAO <kirilichev.a@icloud.com>
2022-08-29 10:22:18 +0700

    38

---
+++ 38/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] arrNum = new double[5];
+for (int i = 0; i < arrNum.Length; i++)
+{
+    arrNum[i] = new Random().Next(1, 10) * 0.1;
+    Console.Write($"{arrNum[i]} ");
+}
+
+double maxNum = arrNum[0];
+double minNum = arrNum[0];
+
+for (int i = 1; i < arrNum.Length; i++)
+{
+    if (maxNum < arrNum[i]) maxNum = arrNum[i];
+    if (minNum > arrNum[i]) minNum = arrNum[i];
+}
+
+double diff = maxNum - minNum;
+
+Console.WriteLine($"-> {diff}");
