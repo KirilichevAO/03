bd9ed295766dcdd758fb057e7bf106e60d7cdc5a
KirilichevAO <kirilichev.a@icloud.com>
2022-08-30 13:39:50 +0700

    44

--- 44/Program.cs
+++ 44/Program.cs
@@ -3,4 +3,26 @@
 // Если N = 3 -> 0 1 1
 // Если N = 7 -> 0 1 1 2 3 5 8
 
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
+void Fibonacci(int number)
+{
+    int prevPrev = 0;
+    int prev = 1;
+
+    Console.Write($"{prevPrev} {prev} ");
+
+    for (int i = 3; i <= number; i++)
+    {
+        int result = prev + prevPrev;
+
+        Console.Write($"{result} ");
+
+        prevPrev = prev;
+        prev = result;
+    }
+}
+
+Fibonacci(n);
+Console.WriteLine();
