8c83e3b65fd4432100db11856e7c3e4d774704f2
KirilichevAO <kirilichev.a@icloud.com>
2022-09-06 17:24:24 +0700

    67

---
+++ 67/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Write("Введите натуральное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int sum = DigitsSum(m);
+
+int DigitsSum(int number)
+{
+    if (number < 10) return number;
+    return number % 10 + DigitsSum(number / 10);
+}
+
+Console.WriteLine($"Сумма цифр = {sum}");
